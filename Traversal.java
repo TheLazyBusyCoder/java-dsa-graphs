ab6d6b7da158deac1c9039cce48568eda126714f
TheLazyBusyCoder <108222281+TheLazyBusyCoder@users.noreply.github.com>
2023-12-09 11:06:44 +0530

    Update Traversal.java

--- Traversal.java
+++ Traversal.java
@@ -1,4 +1,4 @@
-// BFS
+// BFS for joint graph
 public static void bfs(ArrayList<Edge> graph[]) {
 	ArrayDeque<Integer> queue = new ArrayDeque<>();
 	int V = graph.length;
@@ -9,6 +9,37 @@ public static void bfs(ArrayList<Edge> graph[]) {
 		if(!visited[cur]) {
 			System.out.print(cur + " ");
 			visited[cur] = true;
+			ArrayList<Edge> temp = graph[cur];
+			for(Edge e: temp) {
+				queue.add(e.dest);
+			}
+		}
+	}
+}
+
+
+// BFS for disjoint graph
+
+public static void main(String[] args) {
+	int V = 7;
+	ArrayList<Edge> graph[] = new ArrayList[V];
+	createGraph(graph);
+	boolean vis[] = new boolean[V];
+	for(int i = 0; i < V; i++) {
+		if(!vis[i]) {
+			bfs(graph , vis , i);
+		}
+	}
+}
+
+public static void bfs(ArrayList<Edge> graph[] , boolean vis[] , int start) {
+	Queue<Integer> queue = new ArrayDeque<>();
+	queue.add(start);
+	while(!queue.isEmpty()) {
+		int cur = queue.poll();
+		if(!vis[cur]) {
+			System.out.print(cur + " ");
+			vis[cur] = true;
 			ArrayList<Edge> temp = graph[cur];
 			for(Edge e: temp) {
 				queue.add(e.dest);
