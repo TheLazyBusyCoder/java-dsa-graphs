ba0ec02726339e3550b11bb9baafb3596d801136
TheLazyBusyCoder <108222281+TheLazyBusyCoder@users.noreply.github.com>
2023-12-09 19:58:29 +0530

    Update Traversal_DFS.java

--- Traversal_DFS.java
+++ Traversal_DFS.java
@@ -1,23 +1,35 @@
+// DFS with recursion 
+public static void dfs(ArrayList<Edge> graph[] , boolean vis[] , int cur) {
+	System.out.println(cur);
+	vis[cur] = true;
+	
+	for(int i = 0; i < graph[cur].size(); i++) {
+		Edge e = graph[cur].get(i);
+		if(vis[e.dest] == false)
+			dfs(graph , vis , e.dest);
+	}
+}
+
 // DFS with stack
 public static void dfs(ArrayList<Edge> graph[]) {
-		Stack<Integer> stack = new Stack<>();
-		boolean vis[] = new boolean[graph.length];
-		stack.push(0);
-		int cur = 0;
-		while(!stack.isEmpty()) {
-			if(!vis[cur]) {
-				vis[cur] = true;
-				System.out.print(cur + " ");
-			} else {
-				cur = stack.pop();
-			}
-			ArrayList<Edge> temp = graph[cur];
-			for(Edge e: temp) {
-				if(!vis[e.dest]) {
-					stack.push(e.dest);
-					cur = e.dest;
-					break;
-				}
+	Stack<Integer> stack = new Stack<>();
+	boolean vis[] = new boolean[graph.length];
+	stack.push(0);
+	int cur = 0;
+	while(!stack.isEmpty()) {
+		if(!vis[cur]) {
+			vis[cur] = true;
+			System.out.print(cur + " ");
+		} else {
+			cur = stack.pop();
+		}
+		ArrayList<Edge> temp = graph[cur];
+		for(Edge e: temp) {
+			if(!vis[e.dest]) {
+				stack.push(e.dest);
+				cur = e.dest;
+				break;
 			}
 		}
 	}
+}
