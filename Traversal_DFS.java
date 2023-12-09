04ec7a792f9d412c212909e6dba3287d16f8fd43
TheLazyBusyCoder <108222281+TheLazyBusyCoder@users.noreply.github.com>
2023-12-09 12:05:58 +0530

    Create Traversal_DFS.java

---
+++ Traversal_DFS.java
@@ -0,0 +1,23 @@
+// DFS with stack
+public static void dfs(ArrayList<Edge> graph[]) {
+		Stack<Integer> stack = new Stack<>();
+		boolean vis[] = new boolean[graph.length];
+		stack.push(0);
+		int cur = 0;
+		while(!stack.isEmpty()) {
+			if(!vis[cur]) {
+				vis[cur] = true;
+				System.out.print(cur + " ");
+			} else {
+				cur = stack.pop();
+			}
+			ArrayList<Edge> temp = graph[cur];
+			for(Edge e: temp) {
+				if(!vis[e.dest]) {
+					stack.push(e.dest);
+					cur = e.dest;
+					break;
+				}
+			}
+		}
+	}
